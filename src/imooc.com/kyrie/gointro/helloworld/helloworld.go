d8fcd1ed25b89fd4200841d34a26d86cc51a0e09
irying <kyrieup@gmail.com>
2018-02-10 17:31:51 +0800

    hello world

---
+++ src/imooc.com/kyrie/gointro/helloworld/helloworld.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ch := make(chan string)
+	for i :=0 ;i < 5000 ; i++  {
+		go printHelloWorld(i, ch)
+	}
+
+	for {
+		msg := <-ch;
+		fmt.Println(msg)
+	}
+
+}
+func printHelloWorld(i int, ch chan string) {
+	for {
+		ch <- fmt.Sprintf("hello world from goroutine %d!\n", i)
+	}
+}
+
