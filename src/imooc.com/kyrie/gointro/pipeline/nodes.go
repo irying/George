0771629eaac9a4cf226fbfd647abaf80687d9f55
irying <kyrieup@gmail.com>
2018-02-12 00:07:49 +0800

    Read from file

--- src/imooc.com/kyrie/gointro/pipeline/nodes.go
+++ src/imooc.com/kyrie/gointro/pipeline/nodes.go
@@ -4,6 +4,7 @@ import (
 	"sort"
 	"io"
 	"encoding/binary"
+	"math/rand"
 )
 
 func ArraySource( a ...int ) <- chan int {
@@ -74,5 +75,24 @@ func ReaderSource(reader io.Reader) <-chan int {
 		close(out)
 	}()
 	
+	return  out
+}
+
+func WriterSink(writer io.Writer, in <- chan int)  {
+	for v := range in {
+		buffer := make([]byte, 8)
+		binary.BigEndian.PutUint64(buffer, uint64(v))
+		writer.Write(buffer)
+	}
+}
+
+func RandomSource(count int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < count; i++ {
+			out <- rand.Int()
+		}
+		close(out)
+	}()
 	return  out
 }
--- src/imooc.com/kyrie/gointro/pipelineDemo/main.go
+++ src/imooc.com/kyrie/gointro/pipelineDemo/main.go
@@ -3,10 +3,32 @@ package main
 import (
 	"imooc.com/kyrie/gointro/pipeline"
 	"fmt"
+	"os"
 )
 
 func main() {
-	//p := pipeline.ArraySource(3, 2, 6, 7, 4)
+	const filename = "small.in"
+	const count = 50
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	p := pipeline.RandomSource(count)
+	pipeline.WriterSink(file, p)
+
+	file, err = os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	p = pipeline.ReaderSource(file)
+	for v := range p{
+		fmt.Println(v)
+	}
+}
+
+func MergeDemo() {//p := pipeline.ArraySource(3, 2, 6, 7, 4)
 	//for {
 	//	if num, ok := <-p; ok {
 	//		fmt.Println(num)
@@ -21,4 +43,5 @@ func main() {
 	for v := range p {
 		fmt.Println(v)
 	}
+
 }
