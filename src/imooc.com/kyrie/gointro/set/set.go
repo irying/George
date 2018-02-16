aba93c073b9099f6a4d68550eea8c75918e1ccfe
irying <kyrieup@gmail.com>
2018-02-16 11:55:24 +0800

    set

---
+++ src/imooc.com/kyrie/gointro/set/set.go
@@ -0,0 +1,12 @@
+package set
+
+type Set interface {
+	Add(e interface{}) bool
+	Remove(e interface{})
+	Clear()
+	Contains(e interface{}) bool
+	Len() int
+	Same(other Set) bool
+	Elements() []interface{}
+	String() string
+}
