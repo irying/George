7249b24108c19efe6a0e2191bcd9b56570f1f1f8
irying <kyrieup@gmail.com>
2018-03-03 22:27:57 +0800

    实现类型

--- src/imooc.com/kyrie/gointro/mutexCond/cond.go
+++ src/imooc.com/kyrie/gointro/mutexCond/cond.go
@@ -1,5 +1,10 @@
 package mutexCond
 
+import (
+	"os"
+	"sync"
+)
+
 type Data []byte
 
 type DataFile interface {
@@ -10,4 +15,13 @@ type DataFile interface {
 	DataLen() uint32
 }
 
-
+type myDataFile struct {
+	file *os.File
+	fmutex sync.RWMutex //被用于文件的读写锁
+	rcond *sync.Cond // 读操作需要的条件变量
+	wmutex sync.Mutex // 写操作需要用到的互斥锁
+	rmutex sync.Mutex // 读操作的互斥锁
+	woffset int64
+	roffset int64
+	dataLen uint32
+}
